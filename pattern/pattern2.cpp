2808a3d57947bbb969305adaee7ebad2055751b1
nithin9000 <nithinkrishnalal303@gmail.com>
2026-02-25 12:47:41 +0530

    pattern

---
+++ pattern/pattern2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=0;j<i;j++){
+            cout<<'*';
+        }
+        cout<<"\n";
+    }
+}
