b3908a0997505f8385759c5cba22954feeeffae0
nithin9000 <nithinkrishnalal303@gmail.com>
2026-03-23 21:36:18 +0530

    recurssion

---
+++ recurssion/n_times.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n_times(int n){
+    cout<<"Nithin"<<"\n";
+    if(n==1){
+        return 0;
+    }
+    else{
+        n_times(n-1);
+    }
+    return 0;
+}
+int main(){
+    int n;
+    cin >> n;
+    n_times(n);
+}
